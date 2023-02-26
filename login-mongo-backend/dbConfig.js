66c970d5fa77790e1eb39b32a3a235a51f987c82
caleb9083 <calebosam12@gmail.com>
2023-02-26 13:26:53 +0000

    Set local connection string

--- login-mongo-backend/dbConfig.js
+++ login-mongo-backend/dbConfig.js
@@ -2,10 +2,14 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 dotenv.config();
 
+//Remote Database
 const DB = process.env.DATABASE_CONNECTION_STRING.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
+
+//Local Database
+// const DB = `mongodb://localhost:27017/mydatabase`
 
 mongoose.set("strictQuery", true);
 
